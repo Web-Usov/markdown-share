2b066d5cd5189cfa695acab67cf99810ee9a96bf
Alex Usov <web.usov@ya.ru>
2025-02-18 02:31:16 +0400

    useCachedState updated

--- src/utils/useCachedState.tsx
+++ src/utils/useCachedState.tsx
@@ -3,7 +3,19 @@ import { useEffect, useState } from "react";
 export function useCachedState<T>(key: string, initialValue: T) {
   const [state, setState] = useState<T>(() => {
     const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initialValue;
+    if (!storedValue || storedValue === "undefined") {
+      return initialValue;
+    }
+    try {
+      const parsedValue = JSON.parse(storedValue);
+      return parsedValue;
+    } catch (error) {
+      console.error(
+        `Failed to parse cached state: ${key}-${storedValue}`,
+        error
+      );
+      return initialValue;
+    }
   });
 
   useEffect(() => {
